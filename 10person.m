40c566416e4c1788cb25d47ca72b238796e272e8
Ching-Best <ljy_csu2017@outlook.com>
2018-11-04 20:44:55 -0700

    add person identifier vector to data

--- 10person.m
+++ 10person.m
@@ -3,22 +3,27 @@ for i=1:10
     i
     for j=1:65               
         a=imread(strcat('/Users/lijingya/Desktop/mathematics_project/CroppedYale/0',num2str(i),'/','0',num2str(j),'.pgm'));
-        b=a(1:192*168);      
+        b=a(1:192*168);  % N=32256    
         j                   
         b=double(b);
-        b = imresize(b,[64,64]);
-        training_set=[training_set;b];
+        training_set=[training_set;b]; %M*N matrix M=650 the number of picture
         
     end
-end
-Mean=mean(training_set);    
+end   
 X = zeros(size(training_set));
+Mean=mean(training_set); % 1*N column vector
 for i=1:10
-    X(i,:)=training_set(i,:)-Mean;
+    X(i,:)=training_set(i,:)-Mean; % X is an M*N matrix, every row stands for every picture - meanpicture
+    
 end
-figure;
-subplot(1,2,1),imshow(mat2gray(reshape(Mean,192,168)));
-title('Mean');   
-subplot(1,2,2),imshow(mat2gray(reshape(X(1,:),192,168))); 
-title('S1P1-Mean');
+figure; %Average face
+subplot(1,2,1),imshow(mat2gray(reshape(Mean,192,168))); title('Mean');  %Average face 
+subplot(1,2,2),imshow(mat2gray(reshape(X(1,:),192,168))); title('S1P1-Mean');
+
+
+for n=1:10
+
+        eval(['a',num2str(n),'=','training_set(n:65:650,:,:)',';']);
+
+end
 
